c6e99ba843fe6158b97c2e3d44a33e92a4c3a4af
Ionuț Arțăriși <ionut@artarisi.eu>
2013-04-20 15:57:14 +0200

    basic compojure+ring webserver

--- project.clj
+++ project.clj
@@ -7,5 +7,11 @@
                  [korma "0.3.0-RC5"]
                  [org.postgresql/postgresql "9.2-1002-jdbc4"]
                  [org.clojure/java.jdbc "0.3.0-alpha1"]
-                 [org.webbitserver/webbit "0.4.14"]]
-  :main greybear.server)
+                 [org.webbitserver/webbit "0.4.14"]
+                 [ring/ring-core "1.1.8"]
+                 [ring/ring-jetty-adapter "1.1.8"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.3"]]
+  :plugins [[lein-ring "0.8.3"]]
+  :main greybear.server
+  :ring {:handler greybear.server/app})
--- src/greybear/server.clj
+++ src/greybear/server.clj
@@ -1,7 +1,27 @@
 (ns greybear.server
   (:gen-class)
   (:import [org.webbitserver WebServer WebServers WebSocketHandler]
-            [org.webbitserver.handler StaticFileHandler]))
+            [org.webbitserver.handler StaticFileHandler])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler])
+  (:use [ring.adapter.jetty]
+        [hiccup.core]
+        [hiccup.middleware :only [wrap-base-url]]
+        [compojure.core :only [defroutes GET]]))
+
+
+(defn index-page []
+  (html
+   [:head [:title "CACA"]]))
+
+(defroutes main-routes
+  (GET "/" [] (index-page))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def app
+  (-> (handler/site main-routes)
+      (wrap-base-url)))
 
 (defn -main
   [& args]
