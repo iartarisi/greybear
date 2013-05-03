88c6a1fb5a179eb11764ed76200b619cec03a12c
Ionuț Arțăriși <ionut@artarisi.eu>
2013-05-03 18:42:13 +0300

    sample websocket client code

--- src/cljs/greybear.cljs
+++ src/cljs/greybear.cljs
@@ -1,2 +1,12 @@
 (ns greybear
-  (:require [goboard]))
+  (:require [goog.events]
+            [goog.net.WebSocket]
+            [goboard]))
+
+(defn on-open
+  [event]
+  (.log js/console "Opened!" event))
+
+(doto (goog.net.WebSocket.)
+  (goog.events/listen (.-OPENED goog.net.WebSocket/EventType) on-open)
+  (.open "ws://localhost:8080/websocket"))
