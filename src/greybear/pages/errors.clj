ef1d42d4d2d38bb67049d6dd31d73da4f12eb8f1
Ionuț Arțăriși <ionut@artarisi.eu>
2013-05-06 20:07:36 +0300

    handle game not found error

--- src/greybear/pages/errors.clj
+++ src/greybear/pages/errors.clj
@@ -4,3 +4,7 @@
 (def failed-authentication
   {:status 401
    :body (html5 "Failed authentication.")})
+
+(def game-not-found
+  {:status 404
+   :body (html5 (format "Game not found: %s." game-id))})
--- src/greybear/pages/game.clj
+++ src/greybear/pages/game.clj
@@ -1,6 +1,7 @@
 (ns greybear.pages.game
   (:use [hiccup element page]
-        [greybear.model :only [read-game]]))
+        [greybear.model :only [read-game]]
+        [greybear.pages.errors :only [game-not-found]]))
 
 (defn- stones-to-js
   "Transforms a string of chars into a JSON array
@@ -13,15 +14,17 @@
   (let [game (read-game game-id)
         count (:count session 0)
         session (assoc session :count (inc count))]
-    {:session session
-     :body
-     (html5
-      [:head
-       [:title "Grey Bear"]
-       (include-js "/js/greybear.js")]
-      [:body
-       [:div#players "Players: " (game :white) " vs. " (game :black)]
-       [:div#caca "Username: " session]
-       [:canvas#goBoard]
-       (javascript-tag (format "goboard.draw(\"goBoard\", %s, 1, function(x, y) {console.log(x, y)}, 18, 17);"
-                               (stones-to-js (game :stones))))])}))
+    (if game
+      {:session session
+       :body
+       (html5
+        [:head
+         [:title "Grey Bear"]
+         (include-js "/js/greybear.js")]
+        [:body
+         [:div#players "Players: " (game :white) " vs. " (game :black)]
+         [:div#caca "Username: " session]
+         [:canvas#goBoard]
+         (javascript-tag (format "goboard.draw(\"goBoard\", %s, 1, function(x, y) {console.log(x, y)}, 18, 17);"
+                                 (stones-to-js (game :stones))))])}
+      game-not-found)))
