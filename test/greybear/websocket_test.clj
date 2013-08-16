569b635af788b8f7fc9ea5a359f4edcd2433a028
Ionuț Arțăriși <ionut@artarisi.eu>
2013-08-16 20:40:07 +0200

    indentation

--- test/greybear/websocket_test.clj
+++ test/greybear/websocket_test.clj
@@ -4,7 +4,11 @@
         [greybear.model :only [make-move]]))
 
 (fact "on-message make-move makes a new move and calls refresh"
-  (let [message "{\"cmd\": \"make-move\", \"game_id\": 142, \"user_id\": 3, \"x\": 1, \"y\": 3}"]
+  (let [message (str "{\"cmd\": \"make-move\", "
+                     "\"game_id\": 142, "
+                     "\"user_id\": 3,"
+                     "\"x\": 1,"
+                     "\"y\": 3}")]
     (on-message nil message) => nil
     (provided
       (make-move 142 "1-3") => anything
