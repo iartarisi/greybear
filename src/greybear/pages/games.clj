6c07347294ea8dd9426919bfe32a09b86d6ef46f
Ionuț Arțăriși <ionut@artarisi.eu>
2013-09-10 23:46:25 +0300

    create a new game form with broken link

--- src/greybear/pages/games.clj
+++ src/greybear/pages/games.clj
@@ -3,20 +3,23 @@
         [greybear.pages.layout :only [base-layout]]
         [greybear.model :only [games-list]]))
 
+(defn games-partial []
+  [:div.row
+   [:div.col-md-2]
+   [:div.col-md-8
+    [:table#games.table.table-condensed.table-hover
+     [:thead [:tr
+              [:th "#"] [:th "White"] [:th "Black"] [:th "Move"]]]
+     [:tbody
+      (for [game (games-list)]
+        [:tr {:onclick
+              (str "location.href='/games/" (:id game) "';")}
+         [:td (:id game)]
+         [:td (:white_id game)]
+         [:td (:black_id game)]
+         [:td (:moves game)]])]]]
+   [:div.col-md-2]])
+
 (defn games [request]
   (base-layout "Games" request
-               [:div.row
-                [:div.col-md-2]
-                [:div.col-md-8
-                 [:table#games.table.table-condensed.table-hover
-                  [:thead [:tr
-                           [:th "#"] [:th "White"] [:th "Black"] [:th "Move"]]]
-                  [:tbody
-                   (for [game (games-list)]
-                     [:tr {:onclick
-                           (str "location.href='/games/" (:id game) "';")}
-                      [:td (:id game)]
-                      [:td (:white_id game)]
-                      [:td (:black_id game)]
-                      [:td (:moves game)]])]]]
-                [:div.col-md-2]]))
+               (games-partial)))
--- src/greybear/pages/new_game.clj
+++ src/greybear/pages/new_game.clj
@@ -1,8 +1,17 @@
 (ns greybear.pages.new-game
   (:require [cemerick.friend :as friend])
-  (:use [greybear.pages.layout :only [base-layout]]))
+  (:use [greybear.pages.helpers :only [get-user-id]]   
+        [greybear.pages.layout :only [base-layout]]
+        [greybear.pages.games :only [games-partial]]))
 
 (defn new-game [request]
   (friend/authenticated
    (base-layout "Start a new game" request
-                "OOO")))
+                [:div.row
+                 [:div.col-md-5]
+                 [:div.col-md-2
+                  [:form {:method "post" :action "/new-game"}
+                   [:input {:type "hidden" :name "user-id"
+                            :value (get-user-id request)}]
+                   [:button.btn.btn-success "New Game"]]]
+                 [:div.col-md-5]])))
