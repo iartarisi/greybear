e5f173165670d8782cdd06138e29b924dd4e1bd9
Ionuț Arțăriși <ionut@artarisi.eu>
2014-06-29 14:42:14 +0200

    fix licensing/description project metadata

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject greybear "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Live Go game server using HTML5 canvas and websockets"
+  :url "http://github.com/mapleoin/greybear"
+  :license {:name "GNU Affero General Public License version 3"
+            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[com.cemerick/friend "0.1.5"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.3"]
